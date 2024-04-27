5940430870e415d4b9fb00205215989d476d3538
jinny-l <jinny.l.dev@gmail.com>
2024-04-28 02:42:07 +0900

    refactor: 회원가입 실패 시, 401 응답코드를 400으로 수정

--- src/main/java/com/commerce/member/api/exception/MemberException.java
+++ src/main/java/com/commerce/member/api/exception/MemberException.java
@@ -9,9 +9,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum MemberException implements CustomException {
 
-    DUPLICATED_LOGIN_ID(HttpStatus.UNAUTHORIZED, "이미 가입된 ID입니다."),
-    DUPLICATED_EMAIL(HttpStatus.UNAUTHORIZED, "이미 가입된 이메일입니다."),
-    DUPLICATED_NICKNAME(HttpStatus.UNAUTHORIZED, "닉네임이 이미 존재합니다."),
+    DUPLICATED_LOGIN_ID(HttpStatus.BAD_REQUEST, "이미 가입된 ID입니다."),
+    DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 가입된 이메일입니다."),
+    DUPLICATED_NICKNAME(HttpStatus.BAD_REQUEST, "닉네임이 이미 존재합니다."),
     LOGIN_ID_NOT_FOUND(HttpStatus.BAD_REQUEST, "잘못된 로그인 ID입니다.");
 
     private final HttpStatus httpStatus;
